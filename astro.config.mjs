68000a9918472f4836d2d873d29cdee2afd0eab4
miulerbm <miulerbm00@gmail.com>
2024-04-30 17:48:11 -0500

    Updating astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-blog-cip.netlify.app",
+  site: "https://miuler-blog.vercel.app/",
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
